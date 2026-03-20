066cac2080d9d3229452e4f060377aaaa2b04e3a
Brat1shkaKERCIX <sasaslobodan174@gmail.com>
2026-03-20 23:07:58 +0200

    Create icons.lua

---
+++ src/elements/icons.lua
@@ -0,0 +1,9 @@
+local Icons = {
+    Home = "rbxassetid://10723346958",
+    Settings = "rbxassetid://10723351001",
+    User = "rbxassetid://10723353456",
+    Folder = "rbxassetid://10723343603",
+    ChevronRight = "rbxassetid://10723342353"
+}
+
+return Icons
